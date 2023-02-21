ca2fd5aab3e2e2e25b431e0293223df22ea517ac
meryvl <meryvalerolaguna@gmail.com>
2023-02-21 18:55:57 +0000

    test jest describe

--- app.js
+++ app.js
@@ -14,6 +14,12 @@ const ONE_EURO_IS = {
     USD: 1.2, // us dollar
     GBP: 0.8, // british pound
 }
+
+const fromEuroToDollar = (euros) =>{
+    return euros * ONE_EURO_IS.USD;
+}
+console.log(fromEuroToDollar(1));
+
 const fromDollarToEuro = (dollars) => dollars / ONE_EURO_IS.USD;
 console.log(fromDollarToEuro(1.2));
 
@@ -21,16 +27,13 @@ const fromDollarToYen = (dollars) => {
 const euros = fromDollarToEuro(dollars);
 return euros * ONE_EURO_IS.JPY;
 };
-console.log(fromDollarToYen(1));
 
-const fromEuroToDollar = (euros) =>{
-    return euros * ONE_EURO_IS.USD;
-}
-console.log(fromEuroToDollar(1));
+console.log(fromDollarToYen(1.2));
+
 const fromYenToPound = (yen) =>{
     const euroJPY = yen / ONE_EURO_IS.JPY;
     return euroJPY * ONE_EURO_IS.GBP;
 };
 console.log(fromYenToPound(1));
 
-module.exports = { sum, fromEuroToDollar }
+module.exports = { sum, fromEuroToDollar, fromDollarToEuro, fromDollarToYen ,fromYenToPound  }
--- app.test.js
+++ app.test.js
@@ -1,20 +1,3 @@
-const { fromEuroToDollar } = require('./app.js');
-describe('app.js', ()=>{
-test("One euro should be 1.2 dollars", function(){
-    expect(fromEuroToDollar(3.5)).toBe(4.2)
-});
- test("This is a function",function(){   
-    const result = typeof(fromEuroToDollar) ==='function';
-    expect(result ).toBe(true);
- });
- test("The result of the function is a number", function(){
-
-const number = !isNaN(fromEuroToDollar(3.5));
-expect(number).toBe(true);
- })
-   
-})
-
 
 const { sum } = require('./app.js');
 describe('app.js', ()=>{
@@ -24,12 +7,61 @@ test("adds 7 + 3 to equal 10", () => {
 test("The result of the function is a number", ()=>{
     const number = !isNaN(sum(7,3));
     expect(number).toBe(true)
-
 });
 test("The result of the function is a whole number", ()=>{
     const numberSum = sum(7,3);
     const wholeNumber  = numberSum % 1 == 0;
     expect(wholeNumber).toBe(true); 
 });
+});
+
+
+
+const { fromEuroToDollar } = require('./app.js');
+describe('app.js', ()=>{
+    test("One euro should be 1.2 dollars", function(){
+        expect(fromEuroToDollar(3.5)).toBe(4.2)
+});
+    test("This is a function",function(){   
+        const result = typeof(fromEuroToDollar) ==='function';
+        expect(result ).toBe(true);
+});
+    test("The result of the function is a number", function(){
+    const number = !isNaN(fromEuroToDollar(3.5));
+    expect(number).toBe(true);
+ })
+   
+})
+
+const { fromDollarToEuro } = require('./app.js');
+describe('app.js', ()=>{
+    test("1.2 Dollar should be 1 euro", function(){
+        expect(fromDollarToEuro(1.2)).toBe(1);
+    })
+    test("This is a function",function(){   
+        const result = typeof(fromDollarToEuro) ==='function';
+        expect(result ).toBe(true);
+     });
+    test("The result of the function is a number", function(){
+        const number = !isNaN(fromDollarToEuro(1.2));
+        expect(number).toBe(true);
+     })  
+});
+
+const {fromDollarToYen} =  require('./app.js')
+describe('app.js', ()=>{
+
+ test("1.2 Dollar should be 127.9 yen", function(){
+    const yen = fromDollarToEuro(1.2) *  127.9
+    expect(fromDollarToYen(1.2)).toBe(127.9);
+})
+test("This is a function",function(){   
+    const result = typeof(fromDollarToYen) ==='function';
+    expect(result ).toBe(true);
+});
+test("The result of the function is a number", function(){
+const number = !isNaN(fromDollarToYen(1.2));
+expect(number).toBe(true);
+})
 
 });
